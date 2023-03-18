1686fe8ed7eefbbf09e07c6f9fdb3aba754a658c
Mehradi <mehradi.info@gmail.com>
2023-03-18 15:31:45 +0100

    Adding Modal viedo in portfolio section

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -1,11 +1,21 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+import ModalVideo from "react-modal-video";
+import "node_modules/react-modal-video/scss/modal-video.scss";
 interface P {
   sctionRef: React.LegacyRef<HTMLElement> | undefined;
 }
 const Portfolio: FC<P> = ({ sctionRef }) => {
+  const [urlVideo, setUrlVideo] = useState("");
   return (
     <section ref={sctionRef} id="portfolio" className="min-h-screen pt-20">
+      <ModalVideo
+        channel="youtube"
+        autoplay
+        isOpen={urlVideo !== ""}
+        videoId={urlVideo}
+        onClose={() => setUrlVideo("")}
+      />
       <div className="flex flex-col justify-center ">
         <h1 className="text-xl font-bold mx-auto w-full p-5 mb-10 text-center ">
           Recent projects
@@ -15,7 +25,12 @@ const Portfolio: FC<P> = ({ sctionRef }) => {
             columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}
           >
             <Masonry gutter="10px">
-              <div className="w-60 h-80 border-4 border-gray-300 ">A </div>
+              <div
+                className="w-60 h-80 border-4 border-gray-300 "
+                onClick={() => setUrlVideo("L61p2uyiMSo")}
+              >
+                View video
+              </div>
               <div className="w-60 h-60 border-4 border-gray-300 ">B </div>
               <div className="w-60 h-40 border-4 border-gray-300 ">Y </div>
               <div className="w-60 h-60 border-4 border-gray-300 ">Z </div>
