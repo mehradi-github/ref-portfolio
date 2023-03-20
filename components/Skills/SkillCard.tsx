a5dc5d67d4dad29b833215f15d638af70ae363d5
Mehradi <mehradi.info@gmail.com>
2023-03-20 10:17:21 +0100

    Adding SkillCard

---
+++ components/Skills/SkillCard.tsx
@@ -0,0 +1,28 @@
+import React, { FC } from "react";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar/";
+import "react-circular-progressbar/dist/styles.css";
+interface P {
+  percentage: number;
+}
+const SkillCard: FC<P> = ({ percentage }) => {
+  return (
+    <div className="flex flex-col justify-center items-start w-full sm:w-5/12 md:w-3/12 xl:w-2/12  h-full border-[1px] border-solid border-gray-300 shadow-xl m-2">
+      <div className="mb-10 mt-5 self-center w-2/3 h-auto">
+        <CircularProgressbar
+          value={percentage}
+          text={`${percentage}%`}
+          strokeWidth={5}
+          styles={buildStyles({
+            rotation: 0,
+            textSize: "20px",
+            pathTransitionDuration: 1,
+            pathColor: "#aa6ffa",
+            textColor: "#051441",
+            trailColor: "#f1f1fa",
+          })}
+        />
+      </div>
+    </div>
+  );
+};
+export default SkillCard;
--- components/Skills/index.tsx
+++ components/Skills/index.tsx
@@ -1,4 +1,5 @@
 import React, { FC, Fragment } from "react";
+import Progressbar from "./SkillCard";
 import SkillSlider from "./SkillSlider";
 
 interface P {
@@ -10,6 +11,19 @@ const Skills: FC<P> = ({ sctionRef }) => {
       <h1 className="text-xl font-bold mx-auto w-full p-5 mb-10 text-center ">
         Skills
       </h1>
+      <div className=" w-full h-auto container mx-auto">
+        <div className=" flex flex-row justify-center items-center flex-wrap ">
+          {[
+            { percentage: 60 },
+            { percentage: 85 },
+            { percentage: 95 },
+            { percentage: 92 },
+            { percentage: 57 },
+          ].map((v, i) => (
+            <Progressbar key={i} percentage={v.percentage} />
+          ))}
+        </div>
+      </div>
       <SkillSlider />
     </section>
   );
