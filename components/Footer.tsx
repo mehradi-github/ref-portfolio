6944eb55fed2d2016938a016a208da8a3f21aa74
Mehradi <mehradi.info@gmail.com>
2023-03-21 19:39:15 +0100

    Adding Footer title

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,7 +10,10 @@ const Footer: FC<P> = ({ sctionRef }) => {
       id="contact"
       className="bg-[url('/assets/images/bg.svg')] bg-no-repeat bg-cover min-h-screen bg-gray-800"
     >
-      Get In Touch
+      <div className=""></div>
+      <div className=" w-1/4 uppercase tracking-wider bg-gradient-to-r from-blue-400 to-pink-500  font-extrabold text-4xl text-white m-5 py-2 px-1 text-center ">
+        Get in touch
+      </div>
     </section>
   );
 };
