08f02433901eae3f201f1384eb8c9f4fd4f56a4b
Mehradi <mehradi.info@gmail.com>
2023-03-22 15:50:59 +0100

    Adding VisibilitySensor in SkillCard

--- components/Skills/SkillCard.tsx
+++ components/Skills/SkillCard.tsx
@@ -1,6 +1,7 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar/";
 import "react-circular-progressbar/dist/styles.css";
+import VisibilitySensor from "react-visibility-sensor";
 interface P {
   title: string;
   items: string[];
@@ -8,22 +9,30 @@ interface P {
   pathColor?: string;
 }
 const SkillCard: FC<P> = ({ title, items, percentage, pathColor }) => {
+  const [present, setPresent] = useState(0);
+
   return (
     <div className="flex flex-col justify-center items-start w-full sm:w-5/12 lg:w-3/12 xl:w-2/12  h-full border-[1px] border-solid border-gray-300 shadow-xl m-2">
       <div className="mb-5 mt-10 self-center w-1/3 sm:w-2/3 only-md:w-1/3 h-auto">
-        <CircularProgressbar
-          value={percentage}
-          text={`${percentage}%`}
-          strokeWidth={5}
-          styles={buildStyles({
-            rotation: 0,
-            textSize: "20px",
-            pathTransitionDuration: 1,
-            pathColor,
-            textColor: "#051441",
-            trailColor: "#f1f1fa",
-          })}
-        />
+        <VisibilitySensor
+          onChange={(isVisible: boolean) => {
+            setPresent(isVisible ? percentage : 0);
+          }}
+        >
+          <CircularProgressbar
+            value={present}
+            text={`${present}%`}
+            strokeWidth={5}
+            styles={buildStyles({
+              rotation: 0,
+              textSize: "20px",
+              pathTransitionDuration: 1,
+              pathColor,
+              textColor: "#051441",
+              trailColor: "#f1f1fa",
+            })}
+          />
+        </VisibilitySensor>
       </div>
       <div className="pb-5 flex flex-col px-3 w-full">
         <h1 className="py-5 text-center text-xl font-bold">{title}</h1>
