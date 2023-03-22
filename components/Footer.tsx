e4e9e3c9f9b63fa64a0898ae484cbe14d1a19e7d
Mehradi <mehradi.info@gmail.com>
2023-03-22 12:58:26 +0100

    Fixing Breakpoint in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -26,18 +26,18 @@ const Footer: FC<P> = ({ sctionRef }) => {
       text-white
       "
     >
-      <div className="flex flex-row justify-center items-start pt-14">
-        <div className="w-1/3 uppercase text-4xl tracking-wider  p-5 self-center">
+      <div className="flex flex-col md:flex-row justify-center items-start pt-14">
+        <div className="w-full md:w-1/3 uppercase text-4xl tracking-wider  p-5 self-center">
           <h1 className="w-10/12 bg-gradient-to-r from-blue-400 to-pink-500  font-extrabold mb-0 py-2 px-1 text-center ">
             Get in touch
           </h1>
           <div className={`${Londrina.className}  mt-0`}>and follow me</div>
         </div>
-        <div className="w-1/3">
-          <h2 className="">Contact Me</h2>
-          <div className="pt-5">
+        <div className="w-full md:w-1/3 ">
+          <h2 className="text-center md:text-left">Contact Me</h2>
+          <div className=" p-10 md:p-5 ">
             <ul>
-              <li className="flex flex-row items-center justify-start mt-5">
+              <li className="flex flex-row items-center justify-start  mt-5">
                 <a href="https://join.skype.com/invite/Je4MWRIGs9Nf">
                   <IoLogoSkype className="text-2xl text-white mr-2 inline-block" />
                   Skype: mehradi.skype
@@ -58,9 +58,9 @@ const Footer: FC<P> = ({ sctionRef }) => {
             </ul>
           </div>
         </div>
-        <div className="w-1/3">
-          <h2 className="">Follow Me</h2>
-          <div className="flex flex-row items-center justify-start mt-10 text-4xl text-white">
+        <div className="w-full pt-5 md:pt-0 md:w-1/3">
+          <h2 className="text-center md:text-left">Follow Me</h2>
+          <div className="flex flex-row items-center justify-center  md:justify-start  mt-10 text-4xl text-white">
             <a
               target="_blank"
               rel="noopener noreferrer"
@@ -116,7 +116,8 @@ const Footer: FC<P> = ({ sctionRef }) => {
         <div className="w-full border-t-[1px] border-gray-50 mt-10 border-solid"></div>
 
         <div className="flex flex-row items-center justify-center text-white text-sm pt-5">
-          <VscLaw className="text-lg mx-2" /> MIT license, designed by &nbsp;
+          <VscLaw className="text-lg mx-2 hidden md:inline-block" /> MIT
+          license, designed by &nbsp;
           <a className="text-blue-400" href="https://github.com/mehradi-github">
             Sadiq Mehradi.
           </a>
