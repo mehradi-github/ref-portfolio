e8b27b7c796e4e64076a3658dd4232d8d7a46a17
Mehradi <mehradi.info@gmail.com>
2023-03-22 13:25:45 +0100

    Fixing breakpoint screen and adding container in Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -26,89 +26,95 @@ const Footer: FC<P> = ({ sctionRef }) => {
       text-white
       "
     >
-      <div className="flex flex-col md:flex-row justify-center items-start pt-14">
-        <div className="w-full md:w-1/3 uppercase text-4xl tracking-wider  p-5 self-center">
-          <h1 className="w-10/12 bg-gradient-to-r from-blue-400 to-pink-500  font-extrabold mb-0 py-2 px-1 text-center ">
-            Get in touch
-          </h1>
-          <div className={`${Londrina.className}  mt-0`}>and follow me</div>
-        </div>
-        <div className="w-full md:w-1/3 ">
-          <h2 className="text-center md:text-left">Contact Me</h2>
-          <div className=" p-10 md:p-5 ">
-            <ul>
-              <li className="flex flex-row items-center justify-start  mt-5">
-                <a href="https://join.skype.com/invite/Je4MWRIGs9Nf">
-                  <IoLogoSkype className="text-2xl text-white mr-2 inline-block" />
-                  Skype: mehradi.skype
-                </a>
-              </li>
-              <li className="flex flex-row items-center justify-start mt-5">
-                <a href="https://t.me/smehradi">
-                  <BsTelegram className="text-2xl text-white mr-2 inline-block" />
-                  Telegram: smehradi
-                </a>
-              </li>
-              <li className="flex flex-row items-center justify-start mt-5">
-                <a href="mailto:mehradi.jobs@gmail.com">
-                  <IoMailSharp className="text-2xl text-white mr-2 inline-block" />
-                  Email: mehradi.jobs@gmail.com
-                </a>
-              </li>
-            </ul>
+      <div className="container mx-auto">
+        <div className=" flex flex-col md:flex-row flex-wrap justify-center items-start pt-14">
+          <div className="w-full only-md:w-1/2 lg:w-1/3 uppercase text-3xl tracking-wider  p-5 self-center">
+            <h1 className="w-10/12 bg-gradient-to-r from-blue-400 to-pink-500  font-extrabold mb-0 py-2 px-1 text-center ">
+              Get in touch
+            </h1>
+            <div className={`${Londrina.className}  mt-0`}>and follow me</div>
           </div>
-        </div>
-        <div className="w-full pt-5 md:pt-0 md:w-1/3">
-          <h2 className="text-center md:text-left">Follow Me</h2>
-          <div className="flex flex-row items-center justify-center  md:justify-start  mt-10 text-4xl text-white">
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://github.com/mehradi-github"
-              aria-label="Github"
-            >
-              <IoLogoGithub className=" mr-4" />
-            </a>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.youtube.com/@devtv2404"
-              aria-label="Youtube"
-            >
-              <IoLogoYoutube className=" mr-4" />
-            </a>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.instagram.com/s_mehradi"
-              aria-label="Instagram"
-            >
-              <IoLogoInstagram className=" mr-4" />
-            </a>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.linkedin.com/in/mehradi"
-              aria-label="Linkedin"
-            >
-              <IoLogoLinkedin className=" mr-4" />
-            </a>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.xing.com/profile/Sadiq_Mehradi"
-              aria-label="Xing"
-            >
-              <IoLogoXing className=" mr-4" />
-            </a>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://twitter.com/mehradi_info"
-              aria-label="Twitter"
-            >
-              <IoLogoTwitter className=" mr-4" />
-            </a>
+          <div className="w-full md:w-1/2 lg:w-1/3 ">
+            <h2 className="text-center md:text-left only-md:hidden">
+              Contact Me
+            </h2>
+            <div className=" p-10 md:p-5 ">
+              <ul>
+                <li className="flex flex-row items-center justify-start  mt-5">
+                  <a href="https://join.skype.com/invite/Je4MWRIGs9Nf">
+                    <IoLogoSkype className="text-2xl text-white mr-2 inline-block" />
+                    Skype: mehradi.skype
+                  </a>
+                </li>
+                <li className="flex flex-row items-center justify-start mt-5">
+                  <a href="https://t.me/smehradi">
+                    <BsTelegram className="text-2xl text-white mr-2 inline-block" />
+                    Telegram: smehradi
+                  </a>
+                </li>
+                <li className="flex flex-row items-center justify-start mt-5">
+                  <a href="mailto:mehradi.jobs@gmail.com">
+                    <IoMailSharp className="text-2xl text-white mr-2 inline-block" />
+                    Email: mehradi.jobs@gmail.com
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="w-full pt-5 md:pt-0 lg:w-1/3">
+            <h2 className="text-center md:text-left only-md:hidden">
+              Follow Me
+            </h2>
+            <div className="flex flex-row items-center justify-center  lg:justify-start  mt-10 text-4xl text-white">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/mehradi-github"
+                aria-label="Github"
+              >
+                <IoLogoGithub className=" mr-4" />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.youtube.com/@devtv2404"
+                aria-label="Youtube"
+              >
+                <IoLogoYoutube className=" mr-4" />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.instagram.com/s_mehradi"
+                aria-label="Instagram"
+              >
+                <IoLogoInstagram className=" mr-4" />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.linkedin.com/in/mehradi"
+                aria-label="Linkedin"
+              >
+                <IoLogoLinkedin className=" mr-4" />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.xing.com/profile/Sadiq_Mehradi"
+                aria-label="Xing"
+              >
+                <IoLogoXing className=" mr-4" />
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://twitter.com/mehradi_info"
+                aria-label="Twitter"
+              >
+                <IoLogoTwitter className=" mr-4" />
+              </a>
+            </div>
           </div>
         </div>
       </div>
