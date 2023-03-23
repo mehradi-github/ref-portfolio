c513286e24e73861df19c530580f274685070c18
Mehradi <mehradi.info@gmail.com>
2023-03-23 23:21:46 +0100

    Showing percentage in skillCards via useVisibilitySection

--- components/Skills/SkillCard.tsx
+++ components/Skills/SkillCard.tsx
@@ -1,7 +1,8 @@
-import React, { FC, useState } from "react";
+import useVisibilitySection from "@/hooks/useVisibilitySection";
+import React, { FC, useEffect, useState } from "react";
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar/";
 import "react-circular-progressbar/dist/styles.css";
-import VisibilitySensor from "react-visibility-sensor";
+// import VisibilitySensor from "react-visibility-sensor";
 interface P {
   title: string;
   items: string[];
@@ -10,29 +11,26 @@ interface P {
 }
 const SkillCard: FC<P> = ({ title, items, percentage, pathColor }) => {
   const [present, setPresent] = useState(0);
-
+  let reveal = useVisibilitySection(3);
+  useEffect(() => {
+    setPresent(reveal === "exit" ? 0 : percentage);
+  }, [reveal]);
   return (
     <div className="flex flex-col justify-center items-start w-full sm:w-5/12 lg:w-3/12 xl:w-2/12  h-full border-[1px] border-solid border-gray-300 shadow-xl m-2">
       <div className="mb-5 mt-10 self-center w-1/3 sm:w-2/3 only-md:w-1/3 h-auto">
-        <VisibilitySensor
-          onChange={(isVisible: boolean) => {
-            setPresent(isVisible ? percentage : 0);
-          }}
-        >
-          <CircularProgressbar
-            value={present}
-            text={`${present}%`}
-            strokeWidth={5}
-            styles={buildStyles({
-              rotation: 0,
-              textSize: "20px",
-              pathTransitionDuration: 1,
-              pathColor,
-              textColor: "#051441",
-              trailColor: "#f1f1fa",
-            })}
-          />
-        </VisibilitySensor>
+        <CircularProgressbar
+          value={present}
+          text={`${present}%`}
+          strokeWidth={5}
+          styles={buildStyles({
+            rotation: 0,
+            textSize: "20px",
+            pathTransitionDuration: 1,
+            pathColor,
+            textColor: "#051441",
+            trailColor: "#f1f1fa",
+          })}
+        />
       </div>
       <div className="pb-5 flex flex-col px-3 w-full">
         <h1 className="py-5 text-center text-xl font-bold">{title}</h1>
