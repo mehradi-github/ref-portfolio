6f5882bcf802dd572be7cf2fafffe4664f1b8595
Mehradi <mehradi.info@gmail.com>
2023-03-26 11:41:51 +0200

    Replaceing react-responsive-masonry with grid

--- components/Portfolio/index.tsx
+++ components/Portfolio/index.tsx
@@ -1,5 +1,4 @@
 import React, { FC, useState } from "react";
-import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 import ModalVideo from "react-modal-video";
 import "node_modules/react-modal-video/scss/modal-video.scss";
 import PortfolioItem from "./PortfolioItem";
@@ -21,126 +20,124 @@ const Portfolio: FC<P> = ({ sctionRef }) => {
           Recent projects
         </h1>
         <div className="container mx-auto">
-          <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 900: 2 }}>
-            <Masonry gutter="10px">
-              {[
-                {
-                  title: "Profile",
-                  description: "TypeScript, Next.js and TailwindCSS",
-                  image: "profile.png",
-                  height: 765,
-                  width: 418,
-                  videoUrl: "L61p2uyiMSo",
-                },
-                {
-                  title: "Mappay",
-                  description: "React.js, Redux, Sass",
-                  image: "mappay.png",
-                  height: 765,
-                  width: 418,
-                  videoUrl: "L61p2uyiMSo",
-                },
-                {
-                  title: "Evisa Express",
-                  description: "JavaScript, Html5, CSS3",
-                  image: "evisa-express.png",
-                  height: 765,
-                  width: 418,
-                  videoUrl: "L61p2uyiMSo",
-                },
-                {
-                  title: "Bazarzoom Dashboard",
-                  description: "TypeScript, React.js and Sass",
-                  image: "bazarzoom-dashboard.png",
-                  height: 765,
-                  width: 418,
-                  videoUrl: "L61p2uyiMSo",
-                },
-                {
-                  title: "BazarZoom",
-                  description: "TypeScript, Next.js and TailwindCSS",
-                  image: "bazarzoom.png",
-                  height: 765,
-                  width: 418,
-                  videoUrl: "L61p2uyiMSo",
-                },
-                {
-                  title: "MFA Bot",
-                  description: "JavaScript, Selenium",
-                  image: "mfa.png",
-                  height: 765,
-                  width: 418,
-                  videoUrl: "L61p2uyiMSo",
-                },
-                {
-                  title: "SigmaSaham Bot",
-                  description: "JavaScript, Cypress",
-                  image: "sigmasaham.png",
-                  height: 765,
-                  width: 418,
-                  videoUrl: "L61p2uyiMSo",
-                },
-                {
-                  title: "Real Estate",
-                  description: "React.js, TailwindCSS, Html5, CSS3",
-                  image: "real-estate.png",
-                  height: 765,
-                  width: 418,
-                  videoUrl: "L61p2uyiMSo",
-                },
-                {
-                  title: "Sampna",
-                  description: "JavaScript, JWT, Sass, Html5, CSS3",
-                  image: "sampna.png",
-                  height: 765,
-                  width: 418,
-                  videoUrl: "L61p2uyiMSo",
-                },
-                {
-                  title: "Quran V1",
-                  description: "React.js, Redux, MUI, Html5, CSS3",
-                  image: "fa-quran-v1.png",
-                  height: 765,
-                  width: 418,
-                  videoUrl: "L61p2uyiMSo",
-                },
-                {
-                  title: "Spot",
-                  description: "JavaScript, Sass, Html5, CSS3",
-                  image: "spot.png",
-                  height: 765,
-                  width: 418,
-                  videoUrl: "L61p2uyiMSo",
-                },
-                {
-                  title: "Mailchi",
-                  description: "JavaScript, Sass, Html5, CSS3",
-                  image: "mailchi.png",
-                  height: 765,
-                  width: 418,
-                  videoUrl: "L61p2uyiMSo",
-                },
-                {
-                  title: "Wide",
-                  description: "JavaScript, Sass, Html5, CSS3",
-                  image: "wide.png",
-                  height: 765,
-                  width: 418,
-                  videoUrl: "L61p2uyiMSo",
-                },
-              ].map((v, i) => (
-                <PortfolioItem
-                  key={i}
-                  title={v.title}
-                  description={v.description}
-                  image={v.image}
-                  height={v.height}
-                  width={v.width}
-                  clickPlay={() => setUrlVideo(v.videoUrl)}
-                />
-              ))}
-            </Masonry>
-          </ResponsiveMasonry>
+          <div className="grid  sm:grid-cols-1   md:grid-cols-2 p-10 gap-5">
+            {[
+              {
+                title: "Profile",
+                description: "TypeScript, Next.js and TailwindCSS",
+                image: "profile.png",
+                height: 765,
+                width: 418,
+                videoUrl: "L61p2uyiMSo",
+              },
+              {
+                title: "Mappay",
+                description: "React.js, Redux, Sass",
+                image: "mappay.png",
+                height: 765,
+                width: 418,
+                videoUrl: "L61p2uyiMSo",
+              },
+              {
+                title: "Evisa Express",
+                description: "JavaScript, Html5, CSS3",
+                image: "evisa-express.png",
+                height: 765,
+                width: 418,
+                videoUrl: "L61p2uyiMSo",
+              },
+              {
+                title: "Bazarzoom Dashboard",
+                description: "TypeScript, React.js and Sass",
+                image: "bazarzoom-dashboard.png",
+                height: 765,
+                width: 418,
+                videoUrl: "L61p2uyiMSo",
+              },
+              {
+                title: "BazarZoom",
+                description: "TypeScript, Next.js and TailwindCSS",
+                image: "bazarzoom.png",
+                height: 765,
+                width: 418,
+                videoUrl: "L61p2uyiMSo",
+              },
+              {
+                title: "MFA Bot",
+                description: "JavaScript, Selenium",
+                image: "mfa.png",
+                height: 765,
+                width: 418,
+                videoUrl: "L61p2uyiMSo",
+              },
+              {
+                title: "SigmaSaham Bot",
+                description: "JavaScript, Cypress",
+                image: "sigmasaham.png",
+                height: 765,
+                width: 418,
+                videoUrl: "L61p2uyiMSo",
+              },
+              {
+                title: "Real Estate",
+                description: "React.js, TailwindCSS, Html5, CSS3",
+                image: "real-estate.png",
+                height: 765,
+                width: 418,
+                videoUrl: "L61p2uyiMSo",
+              },
+              {
+                title: "Sampna",
+                description: "JavaScript, JWT, Sass, Html5, CSS3",
+                image: "sampna.png",
+                height: 765,
+                width: 418,
+                videoUrl: "L61p2uyiMSo",
+              },
+              {
+                title: "Quran V1",
+                description: "React.js, Redux, MUI, Html5, CSS3",
+                image: "fa-quran-v1.png",
+                height: 765,
+                width: 418,
+                videoUrl: "L61p2uyiMSo",
+              },
+              {
+                title: "Spot",
+                description: "JavaScript, Sass, Html5, CSS3",
+                image: "spot.png",
+                height: 765,
+                width: 418,
+                videoUrl: "L61p2uyiMSo",
+              },
+              {
+                title: "Mailchi",
+                description: "JavaScript, Sass, Html5, CSS3",
+                image: "mailchi.png",
+                height: 765,
+                width: 418,
+                videoUrl: "L61p2uyiMSo",
+              },
+              {
+                title: "Wide",
+                description: "JavaScript, Sass, Html5, CSS3",
+                image: "wide.png",
+                height: 765,
+                width: 418,
+                videoUrl: "L61p2uyiMSo",
+              },
+            ].map((v, i) => (
+              <PortfolioItem
+                key={i}
+                title={v.title}
+                description={v.description}
+                image={v.image}
+                height={v.height}
+                width={v.width}
+                clickPlay={() => setUrlVideo(v.videoUrl)}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </section>
