5339e58539b02399f606cb07c85581e0201fdcb8
Mehradi <mehradi.info@gmail.com>
2023-03-28 23:18:59 +0200

    Adding Google tag (gtag.js)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,33 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import { ProfileContext, ctx, initialContext } from "@/context/context";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [ctx, setCtx] = useState(initialContext);
 
   return (
-    <ProfileContext.Provider value={[ctx, setCtx]}>
-      <ThemeProvider enableSystem={true} attribute="class">
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </ProfileContext.Provider>
+    <Fragment>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_MEASUREMENT_ID}`}
+      />
+      <Script strategy="lazyOnload" id="">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', '${process.env.GOOGLE_MEASUREMENT_ID}');
+      `}
+      </Script>
+      <ProfileContext.Provider value={[ctx, setCtx]}>
+        <ThemeProvider enableSystem={true} attribute="class">
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </ProfileContext.Provider>
+    </Fragment>
   );
 }
