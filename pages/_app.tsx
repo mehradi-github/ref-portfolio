1f39eba47e2c46946e2cf1f3d970352e64d864f0
Mehradi <mehradi.info@gmail.com>
2023-04-03 12:28:51 +0200

    Adding vercel/analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,19 @@ import { ThemeProvider } from "next-themes";
 import { Fragment, useState } from "react";
 import { ProfileContext, ctx, initialContext } from "@/context/context";
 import Script from "next/script";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [ctx, setCtx] = useState(initialContext);
 
   return (
     <Fragment>
+      <ProfileContext.Provider value={[ctx, setCtx]}>
+        <ThemeProvider enableSystem={true} attribute="class">
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </ProfileContext.Provider>
+
       <Script
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID}`}
@@ -23,11 +30,7 @@ export default function App({ Component, pageProps }: AppProps) {
         gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID}');
       `}
       </Script>
-      <ProfileContext.Provider value={[ctx, setCtx]}>
-        <ThemeProvider enableSystem={true} attribute="class">
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </ProfileContext.Provider>
+      <Analytics />
     </Fragment>
   );
 }
